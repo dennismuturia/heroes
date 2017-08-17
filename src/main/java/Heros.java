b5ca39d72c7caa22a43b98b005bc83ba6732d25e
dennismuturia <dmuturia2922@gmail.com>
2017-08-17 15:14:33 +0300

    Finished up with layout

--- src/main/java/Heros.java
+++ src/main/java/Heros.java
@@ -1,11 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Heros{
   //Initialize class Variables for the Heros
-  private hName;
-  private hAge;
-  private hSuperpower
+  private String hName;
+  private int hAge;
+  private List<String> hSuperpower;
+  private List<String> hWeakness;
 
   public Heros(String name){
-
+    hName = name;
   }
-  
+  public String getName(){
+    return hName;
+  }
+  public int getAge(int Age){
+    hAge = Age;
+    return hAge;
+  }
+  public List<String> getSuperPower(){
+    hSuperpower = new ArrayList<String>();
+    return hSuperpower;
+  }
+  public List<String> getWeakness(){
+    hWeakness = new ArrayList<String>();
+    return hWeakness;
+  }
 }
--- src/test/java/HerosTest.java
+++ src/test/java/HerosTest.java
@@ -7,4 +7,28 @@ public class HerosTest{
     Heros myHero = new Heros("Batman");
     assertEquals(true, myHero instanceof Heros);
   }
+
+  @Test
+  public void hero_correctlyInstatiatesWithName_true(){
+    Heros myHero = new Heros("Batman");
+    assertEquals("Batman", myHero.getName());
+  }
+
+  @Test
+  public void hero_instantiatesCorrectlyEithAge_true(){
+    Heros myHero = new Heros("Batman");
+    assertEquals(45, myHero.getAge(45));
+  }
+
+  @Test
+  public void getArrayList_InstatiatesCorrectlyWithSuperPowers_true(){
+    Heros myHero =new Heros("Batman");
+    assertEquals(0, myHero.getSuperPower().size());
+  }
+
+  @Test
+  public void getArrayList_InstatiatesCorrectlyWithWeakNess_true(){
+    Heros myHero =new Heros("Batman");
+    assertEquals(0, myHero.getWeakness().size());
+  }
 }
