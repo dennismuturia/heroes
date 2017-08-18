857ac9beb73c3825fa0b154f4b87e852b1aa491d
dennismuturia <dmuturia2922@gmail.com>
2017-08-18 11:05:48 +0300

    Added a few styling features and finishing up tests

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,6 +1,32 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Squad{
-  
+  private String sName;
+  private int sId;
+  private static List<Squad> mySquads = new ArrayList<Squad>();
+  private String[] sHeroes = new String[4];
+
+  public Squad(String name){
+    mySquads.add(this);
+    sName = name;
+    sId = mySquads.size();
+
+  }
+  public String getName(){
+    return sName;
+  }
+  public int getId(){
+    return sId;
+  }
+  public static List<Squad> all(){
+    return mySquads;
+  }
+  public static void clear(){
+    mySquads.clear();
+  }
+  public int getMaxSize(){
+    return sHeroes.length;
+  }
 }
--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -2,5 +2,36 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class SquadTest{
+  @Test
+  public void checkWhetherSquadsInstantiatesCorrectly_true(){
+    Squad mySquad = new Squad("Killer_Squad");
+    assertEquals(true, mySquad instanceof Squad);
+  }
 
+  @Test
+  public void getName_checkWhetherItInstantiateswithName_true(){
+    Squad mySquad = new Squad("Killer_Squad");
+    assertEquals("Killer_Squad", mySquad.getName());
+  }
+
+  @Test
+  public void returnsAllTheSquadsinAList_true(){
+    Squad mySquad1 = new Squad("Killer_Squad");
+    Squad mySquad2 = new Squad("The_Finishers");
+    assertEquals(true, Squad.all().contains(mySquad1));
+    assertEquals(true, Squad.all().contains(mySquad2));
+  }
+
+  @Test
+  public void getId_ChecksIfInstantiatesWithAnId(){
+    Squad.clear();
+    Squad mySquad = new Squad("Killer_Squad");
+    assertEquals(1, mySquad.getId());
+  }
+
+  @Test
+  public void maxsize_CheckTheMaxSize(){
+    Squad mySquad = new Squad("Killer_Squad");
+    assertEquals(4, mySquad.getMaxSize());
+  }
 }
